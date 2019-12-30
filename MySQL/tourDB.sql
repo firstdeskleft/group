a417db2fabf40239d3f44c3793daca26652862c2
Nick Sampanis <nick.sabanis@gmail.com>
2019-12-30 17:27:29 +0200

    added primitive database, from .msql file

---
+++ MySQL/tourDB.sql
@@ -0,0 +1,36 @@
+drop database if exists tourDB;
+create database tourDB;
+
+use tourDB;
+
+create table Customer(
+cid int unsigned primary key auto_increment,
+firstName varchar (45) not null,
+lastName varchar (45) not null,
+credits int unsigned not null default 0
+);
+
+create table certificate(
+cert_id int unsigned primary key auto_increment,
+title varchar (45) not null
+);
+
+create table guide(
+gid int unsigned primary key auto_increment,
+firstName varchar (45) not null,
+lastName varchar (45) not null,
+profits int unsigned not null default 0,
+subject varchar (45),
+cert_id int unsigned,
+foreign key (cert_id) references certificate(cert_id)
+);
+
+drop table tour;
+create table tour(
+tid int unsigned primary key auto_increment,
+location varchar (45) not null,
+cost int unsigned not null default 0,
+date date not null,
+gid 
+);
+
